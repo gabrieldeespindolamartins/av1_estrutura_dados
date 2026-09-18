f23f04301f5b6d25fcccfb86e2802c8f311205c8
Calegariii <gabrielcalega@gmail.com>
2026-09-18 10:07:40 -0300

    Gerenciamento de notas

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Olá Mundo")
+notas_alunos = 
+
+while > 0:
+
+"""Sistema de Gerenciamento de Notas"""
+print("Bem-Vindo ao Sistema de Gerenciamento de Notas")
+print("1. -----Cadastrar Aluno-----")
+print("2. -----Listar Todos os Alunos-----")
+print("3. -----Buscar Aluno-----")
+print("4. -----Sair do Sistema-----")
+
+
+opcao = input("Escolha uma opção: ")
+
+print("olá")
